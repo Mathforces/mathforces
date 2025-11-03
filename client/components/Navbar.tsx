2cc4002ce1dd6714b42863d4bebd39f65e1966d8
ibrahimwael951 <ibrahimwael809@gmail.com>
2025-11-03 02:08:15 +0200

    improve navbar functionality and responsiveness

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,24 +1,74 @@
+"use client";
 import { MainLinks } from "@/data/Links";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import { Logs } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const pathName = usePathname();
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
   return (
     <nav className="flex justify-between md:justify-evenly items-center gap-5 p-5">
-      <Image src={"/Logo.png"} alt="logo" width={100} height={100} />
+      <Link href="/">
+        <Image
+          src={"/Logo.png"}
+          alt="logo"
+          width={100}
+          height={100}
+          className="bg-black object-contain h-10 px-2 rounded-2xl"
+        />
+      </Link>
       <div className="hidden md:flex">
         {MainLinks.map((link) => (
           <Link
             key={link.name}
             href={link.href}
-            className="mx-3 text-lg hover:text-blue-500 duration-100"
+            className={`mx-3 text-lg hover:text-blue-500 duration-100 ${
+              pathName === link.href ? "text-blue-500 " : "text-gray-700  "
+            }`}
           >
             {link.name}
           </Link>
         ))}
       </div>
-      <Button variant={"primary"}>Sign In</Button>
+
+      <div className="flex items-center gap-2">
+        <Button variant={"primary"}>Sign In</Button>
+        <Button
+          variant={"outline"}
+          onClick={() => setOpenMenu(!openMenu)}
+          className="text-primary hover:text-primary/80 md:hidden"
+        >
+          <Logs size={35} strokeWidth={3} />
+        </Button>
+      </div>
+
+      <div
+        className={`absolute top-20 left-0 w-full bg-background flex flex-col items-center md:hidden border border-gray-200 py-5
+    transition-all duration-300 ease-out
+    ${
+      openMenu
+        ? "opacity-100 translate-y-0"
+        : "opacity-0 -translate-y-5 pointer-events-none"
+    }
+  `}
+      >
+        {MainLinks.map((link) => (
+          <Link
+            key={link.name}
+            href={link.href}
+            onClick={() => setOpenMenu(false)}
+            className={`my-2 text-lg hover:text-blue-500 duration-100 ${
+              pathName === link.href ? "text-blue-500" : "text-gray-700"
+            }`}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 };
